fa4c2e138203c21b40fe419104d0590186f18fe6
Patricio Balboa <pbalboa08@gmail.com>
2025-05-02 02:20:08 -0400

    Resuelto 1

--- ejercicios/ejercicio1.py
+++ ejercicios/ejercicio1.py
@@ -9,16 +9,24 @@
 # Tu código aquí:
 
 # Solicitar el primer número:
-
-
+n1 = int(input("Introduce el primer número: "))
 # Solicitar el segundo número:
-
-
+n2 = int(input("Introduce el segundo número: "))
 # Pedir la operación a realizar:
-
+operacion = input("¿Qué operación deseas realizar (+, -, *, /)? ")
 
 # Calcular el resultado según la operación:
 
+if operacion == '+':
+    resultado = n1 + n2
+elif operacion == '-':
+    resultado = n1 - n2
+elif operacion == '*':
+    resultado = n1 * n2
+elif operacion == '/':
+    resultado = n1 / n2
+else:
+    resultado = "Operación no válida"
 
 # Mostrar el resultado:
-# print("El resultado es:", ...)
+print("El resultado es:", resultado)

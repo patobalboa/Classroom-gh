ebd23a014dcb0410d63d00fcea0ab4090903f76c
Patricio Balboa <pbalboa08@gmail.com>
2025-05-02 02:23:27 -0400

    Resuelto 2

--- ejercicios/ejercicio4.py
+++ ejercicios/ejercicio4.py
@@ -7,16 +7,25 @@
 # Tu código aquí:
 
 # Pedir el primer número:
-
-
+n1 = int(input("Introduce el primer número: "))
 # Pedir el segundo número:
 
-
+n2 = int(input("Introduce el segundo número: "))
 # Pedir el tercer número:
+n3 = int(input("Introduce el tercer número: "))
 
 
 # Comparar los números para saber cuál es el mayor:
-
+if n1 > n2:
+    if n1 > n3:
+        mayor = n1
+    else:
+        mayor = n3
+else:
+    if n2 > n3:
+        mayor = n2
+    else:
+        mayor = n3
 
 # Mostrar el resultado:
-# print("El número mayor es:", ...)
+print("El número mayor es:", mayor)

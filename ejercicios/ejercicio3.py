238526e8ede455659d1148d0e3ca2968029d71cb
Patricio Balboa <pbalboa08@gmail.com>
2025-05-02 02:21:59 -0400

    Resuelto 3

--- ejercicios/ejercicio3.py
+++ ejercicios/ejercicio3.py
@@ -6,11 +6,16 @@
 
 # Tu código aquí:
 
-# Pedir el número:
+# Pedir el número:  
+numero = int(input("Introduce un número entero: "))
 
 
 # Verificar si es par o impar:
 
-
+if numero % 2 == 0:
+    print("El número es par")
+else:
+    print("El número es impar")
+                    
 # Mostrar el mensaje:
 # print("El número es par") o print("El número es impar")
